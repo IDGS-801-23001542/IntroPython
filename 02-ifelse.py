da3b3738807740aeac703e5874982d0d1d9cc7ac
jeezly <heezly123@gmail.com>
2026-01-12 18:11:54 -0600

    Subiendo el trabajo de una vez

---
+++ 02-ifelse.py
@@ -0,0 +1,36 @@
+"""
+Operadores lógicos / relacionales
+
+==  igual
+!=  diferente
+>   mayor que
+<   menor que
+>=  mayor o igual que
+<=  menor o igual que
+
+Operadores aritméticos
+
++   suma
+-   resta
+*   multiplicación
+/   división
+%   módulo
+//  división entera
+**  exponente
+
+Operadores lógicos (booleanos)
+
+and  y
+or   o
+not  no
+"""
+
+
+num1=2
+num2=3
+
+if num1 > num2:    
+    
+    print("{} es mayor que {}".format(num1,num2))
+else:
+    print("{} es menor que {}".format(num1, num2))
